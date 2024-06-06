0d5ed63f50d7f614781f6b616f43d114b35cd859
Walt Wilber <wgwilber@crimson.ua.edu>
2024-06-06 11:59:51 -0500

    push fixed virtual environment

--- crypto-scraper/scraper-env/Scripts/activate.bat
+++ crypto-scraper/scraper-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\UA Documents\REU 2024\Python\data-scraper\scraper-env
+set VIRTUAL_ENV=C:\UA Documents\REU 2024\REU\crypto-scraper\scraper-env
 
 if not defined PROMPT set PROMPT=$P$G
 
